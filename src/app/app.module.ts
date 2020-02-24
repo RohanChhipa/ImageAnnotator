9ebc6c10aa1735a8335a953c499b12ae675de9b6
Rohan <rohan.chhipa@live.com>
2020-02-24 21:10:18 +0200

    Remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AnnotatorComponent } from './annotator/annotator.component';
 import {ImageCanvasComponent} from './image-canvas/image-canvas.component';
 import {DocumentImageListComponent} from './document-image-list/document-image-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AnnotatorComponent,
     ImageCanvasComponent,
     DocumentImageListComponent
   ],
